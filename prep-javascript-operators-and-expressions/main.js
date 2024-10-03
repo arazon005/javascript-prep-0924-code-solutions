bb5ce136b909250088c9d702fe08919fd42915f9
Abraham Razon <arazon005@gmail.com>
2024-10-03 23:50:36 +0000

    Completed prep-javascript-and-expressions

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+const number1 = 2;
+const number2 = 4;
+let product = number1 * number2;
+console.log(product);
+console.log('The data type of product is: ' + typeof product);
+
+let charge = 5;
+let payment = 3;
+let amountRemaining = charge - payment;
+console.log(amountRemaining);
+console.log('The data type of amountRemaining is: ' + typeof amountRemaining);
+
+let tests = 80;
+let assignments = 70;
+let final = 95;
+let grade = (80 + 70 + 95) / 3;
+console.log(grade);
+console.log('The data type of grade is: ' + typeof grade);
+
+let firstName = 'Abraham';
+let lastName = 'Razon';
+let fullName = firstName + ' ' + lastName;
+console.log(fullName);
+console.log('The data type of fullName is: ' + typeof fullName);
+
+let pH = 6;
+let isAcidic = pH > 7;
+console.log(isAcidic);
+console.log('The data type of isAcidic is: ' + typeof isAcidic);
+
+let numberOfSoldiers = 300;
+let isSparta = 300 === numberOfSoldiers;
+console.log(isSparta);
+console.log('The data type of isSparta is: ' + typeof isSparta);
+
+let nickname = fullName;
+nickname += ' is the GOAT';
+console.log(nickname);
+console.log('The data type of nickname is: ' + typeof nickname);
